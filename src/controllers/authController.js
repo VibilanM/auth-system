766c3a47aaa3dfeae4117115883e40912167abdb
Vibilan <vibilan2793@gmail.com>
2026-06-15 21:49:08 +0530

    4/16

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -51,4 +51,46 @@ async function createUser(req, res) {
     }
 }
 
-export default createUser;
+async function loginUser(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "All fields are required."
+            });
+        }
+
+        const user = await User.findOne({
+            email: email.trim().toLowerCase()
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid credentials."
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials."
+            });
+        }
+
+        res.status(200).json({
+            message: "Login successful."
+        });
+    }
+    catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+export { createUser, loginUser };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import createUser from "../controllers/authController.js";
+import { createUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", createUser);
+router.post("/login", loginUser);
 
 export default router;
