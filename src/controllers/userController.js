7da91cb4b33c421ba130553705a08c89b1a96697
Vibilan <vibilan2793@gmail.com>
2026-06-15 19:43:07 +0530

    2/16

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,22 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 async function createUser(req, res) {
     try {
-        const user = await User.create(req.body);
-        res.status(201).json(user);
+        const hashedPassword = await bcrypt.hash(
+            req.body.password,
+            10
+        );
+
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: "User created successfully."
+        });
     } catch (error) {
         res.status(500).json({
             message: error.message
