9738f9d05b6bcd8c187153428ad587fcc9f637f3
Vibilan <vibilan2793@gmail.com>
2026-06-16 18:28:06 +0530

    15/18

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import userRoutes from "./routes/userRoute.js";
 import authRoutes from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
